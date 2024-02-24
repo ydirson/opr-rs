0f69fb02e5b85343ce29817a581a063864a19703
Yann Dirson <ydirson@free.fr>
2024-02-24 19:43:18 +0100

    update-test-data: avoid duplication of path construction

--- update-test-data.sh
+++ update-test-data.sh
@@ -7,5 +7,5 @@ for FILE in "$DATADIR"/armies/????????????; do
     ID=$(basename "$FILE")
     echo "$ID ..."
     curl --silent "https://army-forge.onepagerules.com/api/tts?id=$ID" |
-        json_pp > "$DATADIR/$ID"
+        json_pp > "$FILE"
 done
