12560a448ed2960086e21b5b3854124df71f15a7
Yann Dirson <ydirson@free.fr>
2024-02-24 23:18:36 +0100

    update-test-data.sh: support common-rules and new armies from CLI

--- update-test-data.sh
+++ update-test-data.sh
@@ -3,9 +3,26 @@ set -e
 
 DATADIR="opr-test-data/src/data"
 
-for FILE in "$DATADIR"/armies/????????????; do
-    ID=$(basename "$FILE")
-    echo "$ID ..."
-    curl --silent "https://army-forge.onepagerules.com/api/tts?id=$ID" |
-        json_pp > "$FILE"
+if [ $# = 0 ]; then
+    echo "Updating all data files"
+    set -- "$DATADIR"/armies/*
+fi
+
+for FILE in "$@"; do
+    case "$FILE" in
+        "$DATADIR"/armies/*)
+            ID=$(basename "$FILE")
+            URL="https://army-forge.onepagerules.com/api/tts?id=$ID"
+            ;;
+        "$DATADIR"/common-rules)
+            URL="https://army-forge.onepagerules.com/api/afs/common-rules"
+            ;;
+        *)
+            echo >&2 "ERROR: unrecognized datafile '$FILE'"
+            ;;
+    esac
+    printf "."
+    curl --silent "$URL" | json_pp > "$FILE"
 done
+
+echo
